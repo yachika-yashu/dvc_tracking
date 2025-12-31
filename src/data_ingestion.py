2da5ab9c987b984748df7b273f99f32c12c38dfe
yachika-yashu <Yachikanand@gmail.com>
2025-12-30 23:45:34 -0500

    version 2

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -5,8 +5,8 @@ import os
 df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
 #remove all textual columns
 df = df.iloc[:, 3:]
-#remove data from length of membership column which has length less than 3
-df = df[df['Length of Membership'] > 3]
+#remove data from length of membership column which has length less than 1
+df = df[df['Length of Membership'] > 1]
 
-df.drop(columns=['Avg. Session Length'],inplace=True)
+df.drop(columns=['Time on Website'],inplace=True)
 df.to_csv(os.path.join('data','customer.csv'))
